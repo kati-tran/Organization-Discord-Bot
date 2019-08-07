c6bbd2e9338f12e9aaca04392ee2c5d0ecc26b93
jigglequack <45055772+jigglequack@users.noreply.github.com>
2019-08-07 16:08:00 -0700

    Added bot functionality

---
+++ bot.js
@@ -0,0 +1,27 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring().includes('https:')) {
+            bot.chanels.get('563202381202849832').send(message.substring());
+         };
+});
