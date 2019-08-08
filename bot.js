44ef9cf7eda0db948f5c81f55231a0598ab702c4
jigglequack <45055772+jigglequack@users.noreply.github.com>
2019-08-08 13:11:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ var blockedchannels = ['587909626087866390']
 client.on('message', msg => {
 
   if (msg.content.includes('http') && msg.author != client.user 
-    && !msg.content.includes('!da') && !blockedchannels.includes(msg.channels.id)) {
+    && !msg.content.includes('!da') && !blockedchannels.includes(msg.channel.id)) {
     client.channels.get('563202381202849832').send(msg.author + " linked: " + msg.content);
 
   }
