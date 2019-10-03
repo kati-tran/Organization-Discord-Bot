b483fe2bf8a807641106facb3fe8c0345256a2d3
jigglequack <45055772+jigglequack@users.noreply.github.com>
2019-10-02 18:45:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -154,17 +154,17 @@ client.on('message', msg => {
     }
 
 	function getUserFromMention(mention) {
-		if (!mention) return;
+		// The id is the first and only match found by the RegEx.
+		const matches = mention.match(/^<@!?(\d+)>$/);
 
-		if (mention.startsWith('<@') && mention.endsWith('>')) {
-			mention = mention.slice(2, -1);
+		// If supplied variable was not a mention, matches will be null instead of an array.
+		if (!matches) return;
 
-			if (mention.startsWith('!')) {
-				mention = mention.slice(1);
-			}
+		// However the first element in the matches array will be the entire mention, not just the ID,
+		// so use index 1.
+		const id = matches[1];
 
-			return client.users.get(mention);
-		}
+		return client.users.get(id);
 	}
 
     if(first_word === '!hof' && valid_command > 2)
