14cf3db037bd7d850aa44b2d0a4d464bca11a433
jigglequack <45055772+jigglequack@users.noreply.github.com>
2019-10-02 19:18:42 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,7 +155,7 @@ client.on('message', msg => {
 
     if(first_word === '!hof' && valid_command > 2)
     {
-    	let user = msg.mentions.users.first().id
+    	let user = msg.author
     	console.log(user)
     	if(!user){
     		bot_channel.send("Please pick a valid user to cite. Example: '!hof @User quote quote quote'")
