9ed35c1ece4adeadac99976b33768096bb63bf95
jigglequack <45055772+jigglequack@users.noreply.github.com>
2019-10-11 17:24:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -242,7 +242,7 @@ client.on('message', msg => {
     	msg.channel.send('Testing')
 	  	hall_of_fame.fetchMessages({limit: 100})
 	  		.then(collected =>{
-	  			collected.random(1).forEach(mesg => {
+	  			collected.random(mesg => {
 		  			console.log(mesg)
 		  		})
 	  	  	})
